49e5b841fb75aa258cfaa885d8708e3f526c2381
Bhavin Vadgama <15mca052@nirmauni.ac.in>
2017-10-25 19:55:29 +0530

    Update fetch-album.php

--- php/fetch-album.php
+++ php/fetch-album.php
@@ -1,6 +1,7 @@
 <?php
 
 ini_set('max_execution_time', 0);
+ini_set('request_terminate_timeout', 0);
 require_once '../vendor/autoload.php';
 require_once './facebook_class.php';
 
@@ -11,7 +12,7 @@ if (isset($_SESSION['fb_access_token'])) {
 
     if (isset($_POST['fetch-album'])) {
         $data = json_decode($_POST['fetch-album']);
-        $response = $fb->getAllAlbums($data->id . '?fields=name,photo_count,photos.limit(1000){source}');
+        $response = $fb->getData($data->id . '?fields=name,photo_count,photos.limit(1000){source}');
         echo json_encode($response);
     }
 } else {
