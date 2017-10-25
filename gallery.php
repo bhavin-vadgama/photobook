eabda25c502cd5bbc08818921a09f9b4e4b7fec8
Bhavin Vadgama <15mca052@nirmauni.ac.in>
2017-10-25 19:53:14 +0530

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -1,6 +1,7 @@
 <?php
 
-ini_set('memory_limit', '1024M');
+ini_set('max_execution_time', 0);
+ini_set('request_terminate_timeout', 0);
 if (!isset($_SESSION['fb_access_token'])) {
     header('location: ./');
 }
